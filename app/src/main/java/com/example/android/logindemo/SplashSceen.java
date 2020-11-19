81e4687881937924d7257976d8f3856dc41bb818
Darin Tsui <dtsui@ucsd.edu>
2020-11-18 20:53:56 -0800

    Added splash screen. Added navyblue + gold colors

---
+++ app/src/main/java/com/example/android/logindemo/SplashSceen.java
@@ -0,0 +1,32 @@
+package com.example.android.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashSceen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_sceen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashSceen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
