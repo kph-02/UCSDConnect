59490f936d93408a24558699705b7db5fa2f1dab
Brandon Ho <b1ho@ucsd.edu>
2020-11-21 23:35:59 -0800

    Chat profile pics added

--- app/src/main/java/com/ucsd/connect/demo/Chat/ChatListAdapter.java
+++ app/src/main/java/com/ucsd/connect/demo/Chat/ChatListAdapter.java
@@ -6,14 +6,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 import com.ucsd.connect.demo.ChatActivity;
 import com.ucsd.connect.demo.R;
 import com.ucsd.connect.demo.User.UserProfile;
@@ -23,6 +29,8 @@ import java.util.ArrayList;
 public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder> {
 
     ArrayList<ChatObject> chatList;
+    private FirebaseStorage firebaseStorage;
+    private FirebaseAuth firebaseAuth;
 
     public ChatListAdapter(ArrayList<ChatObject> chatList){
         this.chatList = chatList;
@@ -42,14 +50,27 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatLi
     @Override
     public void onBindViewHolder(@NonNull final ChatListViewHolder holder, final int position) {
         String displayName = "";
+        String chatUid = "";
         ChatObject currChat = chatList.get(position);
         for (UserProfile userIt: currChat.getUserProfileArrayList()) {
             if (!userIt.getUid().equals(FirebaseAuth.getInstance().getUid())) {
                 displayName += userIt.getUserName() + " ";
+                chatUid += userIt.getUid();
             }
         }
 
         holder.mTitle.setText(displayName);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child(chatUid).child("Images/Profile Pic").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).fit().centerCrop().into(holder.mProfilePic);
+            }
+        });
 
         holder.mLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,10 +94,12 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatLi
     public class ChatListViewHolder extends RecyclerView.ViewHolder{
         public TextView mTitle;
         public LinearLayout mLayout;
+        public ImageView mProfilePic;
         public ChatListViewHolder(View view){
             super(view);
             mTitle = view.findViewById(R.id.title);
             mLayout = view.findViewById(R.id.layout);
+            mProfilePic = view.findViewById(R.id.ProfilePic);
         }
     }
 }
